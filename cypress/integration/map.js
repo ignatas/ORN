d31ea1b779d6008b6b39ff7e93b9739f771c9a62
Dmitry Ignatenko <ignatenkodg@gmail.com>
2019-08-18 00:31:36 +0300

    finally map

--- cypress/integration/map.js
+++ cypress/integration/map.js
@@ -2,8 +2,6 @@ let version = Cypress.env('version')
 let agent = Cypress.env('agent')
 let sid = Cypress.env('sid')
 
-var Realm = require('realm')
-
 /*let geoSchema = {
     "type": "FeatureCollection",
     "features": []
@@ -26,88 +24,44 @@ let pointSchema = {
         properties: {
             description: 'string',
             iconCaption: 'string',
-            [marker - color]: 'string'
+            marker: 'string'
         }
     }
 }
 
 describe('map builder', () => {
-Realm.open({ schema: [pointSchema] }).then(realm => {
 
     let i = 0
-    for (i = 0; i < 1000; i++) {
+    for (i = 0; i < 10; i++) {
         it('draw the map iteration', () => {
-
-
             cy.getShops(sid, version, agent)
                 .then(response => {
-                    response.body.result.forEach(shop => {
-                        /*newPoint.type = 'Feature'
-                        newPoint.id = shop.uuid
-                        newPoint.geometry.type = 'Point'
-                        newPoint.geometry.coordinates[0] = shop.location[1]
-                        newPoint.geometry.coordinates[1] = shop.location[0]
-                        newPoint.properties.description = shop.name + ' --lvl= ' + shop.level
-                        newPoint.properties.iconCaption = newPoint.properties.description
-                        newPoint.properties["marker-color"] = '#0000ff'
-
-                        mapdata.features.push(newPoint)*/
-                        try {
-                            realm.write(() => {
-                                realm.create('mapPoint', {
-                                    type: 'Feature',
-                                    id: shop.uuid,
-                                    geometry: {
-                                        type: 'Point',
-                                        coordinates: [
-                                            shop.location[1],
-                                            shop.location[0]
-                                        ]
-                                    },
-                                    properties: {
-                                        description: shop.name + ' --lvl=' + shop.level,
-                                        iconCaption: 'Shop',
-                                        [marker - color]: '#0000ff'
-                                    }
-                                })
-                            })
-                        } catch (e) {
-                            console.log("Error on creation")
-                        }
+                    cy.readFile('cypress/fixtures/shops.csv').then(points => {
+                        response.body.result.forEach(element => {
+                            let point = element.location[0] + ',' + element.location[1] + ',' + element.uuid + ',' + element.name + ' lvl' + element.level + '\n'
+                            if (!points.includes(point)) {
+                                cy.writeFile('cypress/fixtures/shops.csv', point, { flag: 'a+' })
+                            }
+                        })
                     })
                 })
 
-            cy.wait(1000)
+            cy.wait(500)
 
             cy.getBoss(sid, version, agent)
                 .then(response => {
-                    response.body.result.forEach(boss => {
-                        try {
-                            realm.write(() => {
-                                realm.create('mapPoint', {
-                                    type: 'Feature',
-                                    id: boss.uuid,
-                                    geometry: {
-                                        type: 'Point',
-                                        coordinates: [
-                                            boss.location[1],
-                                            boss.location[0]
-                                        ]
-                                    },
-                                    properties: {
-                                        description: boss.name + ' --lvl=' + boss.level,
-                                        iconCaption: 'Boss',
-                                        [marker - color]: '#ff0000'
-                                    }
-                                })
-                            })
-                        } catch (e) {
-                            console.log("Error on creation");
-                        }
+                    cy.readFile('cypress/fixtures/bosses.csv').then(points => {
+                        response.body.result.forEach(element => {
+                            let point = element.location[0] + ',' + element.location[1] + ',' + element.uuid + ',' + element.name + ' lvl' + element.level + '\n'
+                            if (!points.includes(point)) {
+                                cy.writeFile('cypress/fixtures/bosses.csv', point, { flag: 'a+' })
+                            }
+                        })
                     })
                 })
+            cy.wait(5000)
         })
-        cy.wait(5000)
+        // cy.wait(5000)
     }
-})
+
 })
